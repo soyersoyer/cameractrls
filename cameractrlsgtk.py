0a713eb987e2c62a635e7e81db8e450b0c049b44
soyer <soyer@irl.hu>
2022-06-10 14:30:35 +0200

    v0.2.1

--- cameractrlsgtk.py
+++ cameractrlsgtk.py
@@ -8,7 +8,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
 ghurl = 'https://github.com/soyersoyer/cameractrls'
-version = 'v0.2.0'
+version = 'v0.2.1'
 
 v4ldirs = {
     '/dev/v4l/by-id/': '',
