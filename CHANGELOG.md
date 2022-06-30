374318d8ed1a8ab8a4d020fc0b414162042bf996
soyer <soyer@irl.hu>
2022-06-30 12:05:01 +0200

    v0.4.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [0.4.5] - 2022-06-30
+
+### Added
+- AppData for better flatpak integration
+
 ## [0.4.4] - 2022-06-29
 
 ### Fixed
--- cameractrls.py
+++ cameractrls.py
@@ -4,7 +4,7 @@ import ctypes, logging, os.path, getopt, sys, subprocess
 from fcntl import ioctl
 
 ghurl = 'https://github.com/soyersoyer/cameractrls'
-version = 'v0.4.4'
+version = 'v0.4.5'
 
 
 v4ldirs = {
