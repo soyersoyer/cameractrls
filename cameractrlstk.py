e1981a14346ab935bb09c1f2239ffcd435f4bf23
Gergo Koteles <soyer@irl.hu>
2023-05-21 11:48:51 +0200

    cameractrlstk: add orientation handling todo

--- cameractrlstk.py
+++ cameractrlstk.py
@@ -58,10 +58,14 @@ class CameraCtrlsGui:
             self.devicescb['values'] = [d.name for d in self.devices]
 
     def open_camera_window(self):
-        win_width = self.window.winfo_screenwidth() - self.window.winfo_width() - 50
-        win_height = self.window.winfo_height()
         # XXX: HighDPI handling?
-        logging.info(f'open cameraview.py for {self.device.path} with size {win_width}x{win_height}')
+        # XXX: Orientation handling?
+        margin = 50
+        scr_width, scr_height = self.window.winfo_screenwidth(), self.window.winfo_screenheight()
+        win_width, win_height = self.window.winfo_width(), self.window.winfo_height()
+        win_width = scr_width - margin - win_width
+        win_height = scr_height - margin
+        logging.info(f'open cameraview.py for {self.device.path} with max size {win_width}x{win_height} screen size {scr_width}x{scr_height}')
         p = subprocess.Popen([f'{sys.path[0]}/cameraview.py', '-d', self.device.path, '-s', f'{win_width}x{win_height}'])
         self.child_processes.append(p)
 
