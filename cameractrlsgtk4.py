de1b96e869e8ceb5c9da87e4cc460208719984a0
Gergo Koteles <soyer@irl.hu>
2025-10-22 14:34:08 +0200

    cameractrlsgtk4: scale layout fix

--- cameractrlsgtk4.py
+++ cameractrlsgtk4.py
@@ -58,25 +58,14 @@ class CameraCtrlsWindow(Gtk.ApplicationWindow):
             padding-left: 10px;
             padding-right: 10px;
         }
+        /* scale layout fix */
+        scale trough {
+            margin-top: 6px;
+            margin-bottom: -6px;
+        }
         '''
 
         gtk_version = (Gtk.get_major_version(), Gtk.get_minor_version(), Gtk.get_micro_version())
-
-        if gtk_version >= (4, 14, 2):
-            css += '''
-            /* scale layout fix */
-            scale {
-                padding: 8px;
-            }
-            '''
-        else:
-            css += '''
-            /* scale layout fix */
-            scale trough {
-                margin-top: 7px;
-                margin-bottom: -7px;
-            }
-            '''
 
         if gtk_version >= (4, 12, 0):
             css_provider.load_from_string(css)
